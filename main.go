3cf4b92ff8e7ea7dc6016221fc5988068b97df31
david-parkk <ji0513ji@naver.com>
2025-11-24 19:36:45 +0900

    feat: 로또 생성 입출력 추가

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	fmt.Println("hello go")
+	var money int
+	fmt.Println("구입 금액을 입력해 주세요.")
+	_, err := fmt.Scanln(&money)
+	for err != nil {
+		log.Println("[ERROR] 숫자를 입력해 주세요.")
+	}
+	count := money / 1000
+	fmt.Println(count, "개를 구매했습니다.")
+	machine := NewLottoMachine(45, 6)
+	lottos := machine.Generates(count, UserLotto)
+
+	for _, lotto := range lottos {
+		fmt.Println(lotto.String())
+	}
 }
