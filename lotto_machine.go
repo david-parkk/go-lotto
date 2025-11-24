67b68123706a0b8e9590760b11c93e0734e8b030
david-parkk <ji0513ji@naver.com>
2025-11-24 19:02:13 +0900

    feat: 로또 생성기 추가

---
+++ lotto_machine.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type LottoMachine struct {
+	end    int
+	length int
+}
+
+func NewLottoMachine() *LottoMachine {
+	return &LottoMachine{}
+}
+
+func (lottoMachine *LottoMachine) Generates(count int, lottoType LottoType) []Lotto {
+	lotteries := make([]Lotto, 0, count)
+	for i := 0; i < count; i++ {
+		lotteries = append(lotteries, *lottoMachine.Generate(lottoType))
+	}
+	return lotteries
+}
+
+func (lottoMachine *LottoMachine) Generate(lottoType LottoType) *Lotto {
+	rand.Seed(time.Now().UnixNano())
+
+	numMap := make(map[int]bool)
+	numbers := make([]int, 0, 6)
+
+	for len(numbers) < 6 {
+		num := rand.Intn(lottoMachine.end) + 1
+		if !numMap[num] {
+			numMap[num] = true
+			numbers = append(numbers, num)
+		}
+	}
+
+	lotto, _ := NewLotto(numbers, lottoType)
+	return lotto
+}
---
+++ lotto_machine_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLottoMachine_Generate(t *testing.T) {
+	machine := NewLottoMachine()
+	machine.end = 45
+
+	lotto := machine.Generate(UserLotto)
+
+	if len(lotto.Numbers) != 6 {
+		t.Errorf("로또 번호 개수 오류: got %d, want 6", len(lotto.Numbers))
+	}
+
+	numMap := make(map[int]bool)
+	for _, n := range lotto.Numbers {
+		if n < 1 || n > 45 {
+			t.Errorf("로또 번호 범위 오류: got %d", n)
+		}
+		if numMap[n] {
+			t.Errorf("중복된 번호 발견: %d", n)
+		}
+		numMap[n] = true
+	}
+
+	if lotto.Type != UserLotto {
+		t.Errorf("로또 타입 오류: got %v, want %v", lotto.Type, UserLotto)
+	}
+}
+
+func TestLottoMachine_Generates(t *testing.T) {
+	machine := NewLottoMachine()
+	machine.end = 45
+	count := 10
+
+	lottos := machine.Generates(count, UserLotto)
+
+	if len(lottos) != count {
+		t.Errorf("생성된 로또 개수 오류: got %d, want %d", len(lottos), count)
+	}
+
+	for i, lotto := range lottos {
+		if len(lotto.Numbers) != 6 {
+			t.Errorf("[%d] 로또 번호 개수 오류: got %d, want 6", i, len(lotto.Numbers))
+		}
+	}
+}
