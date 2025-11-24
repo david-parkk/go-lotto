b8302544d1cc77fb60b89efc0d7ba082f44def9f
david-parkk <ji0513ji@naver.com>
2025-11-24 19:16:00 +0900

    feat: 당첨 클래스 추가

---
+++ winning_info.go
@@ -0,0 +1,25 @@
+package main
+
+type WinningInfo struct {
+	Rank       int
+	MatchCount int
+	HasBonus   bool
+	Prize      int64
+}
+
+var WinningInfos = []*WinningInfo{
+	{Rank: 1, MatchCount: 6, HasBonus: false, Prize: 2000000000},
+	{Rank: 2, MatchCount: 5, HasBonus: true, Prize: 30000000},
+	{Rank: 3, MatchCount: 5, HasBonus: false, Prize: 1500000},
+	{Rank: 4, MatchCount: 4, HasBonus: false, Prize: 50000},
+	{Rank: 5, MatchCount: 3, HasBonus: false, Prize: 5000},
+}
+
+func NewWinningInfo(rank int, matchCount int, hasBonus bool, prize int64) *WinningInfo {
+	return &WinningInfo{
+		Rank:       rank,
+		MatchCount: matchCount,
+		HasBonus:   hasBonus,
+		Prize:      prize,
+	}
+}
